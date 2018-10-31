491b1e1211f9ce7d1a60fc30e57e8aa195ebd98d
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2018-10-30 23:22:35 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
 
+class Book
 
+#title
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+#author name
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+#page count
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+
+  def page_count
+    @page_count
+  end
+#genre
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
